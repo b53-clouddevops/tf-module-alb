7a800d02b5ee5910058f1c87924aab2d1a54a377
CodingManoj <mvp2612@hotmail.com>
2023-04-28 22:25:59 -0400

    Git Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tf-module-alb
+
+This is the backend module which contains all the alb related terraform code.
---
+++ alb.tf
@@ -0,0 +1,20 @@
+# This creates alb 
+resource "aws_lb" "alb" {
+  name               = var.ALB_NAME
+  internal           = var.INTERNAL 
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
