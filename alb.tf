bc1ee65fdd3e30a3e711788e2191ff37846036fa
CodingManoj <mvp2612@hotmail.com>
2023-04-28 22:36:07 -0400

    Git Auto Push

--- alb.tf
+++ alb.tf
@@ -3,18 +3,13 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL 
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
+  security_groups    = local.SG
+  subnets            = local.SUBNET_IDS
 
   tags = {
-    Environment = "production"
+    Name             = var.ALB_NAME
   }
 }
+
+
+
