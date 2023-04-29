f07c1f4355dc6c90c53348e63c46580f02d563b8
CodingManoj <mvp2612@hotmail.com>
2023-04-28 22:42:20 -0400

    Git Auto Push

--- alb.tf
+++ alb.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "alb" {
   name               = var.ALB_NAME
   internal           = var.INTERNAL 
   load_balancer_type = "application"
-  security_groups    = local.SG
-  subnets            = local.SUBNET_IDS
+  security_groups    = [local.SG]
+  subnets            = [local.SUBNET_IDS]
 
   tags = {
     Name             = var.ALB_NAME
